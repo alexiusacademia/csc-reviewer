a691e4eeb286b2e818361169cfbc4bf696cd637a
alexiusacademia <alexius.academia@gmail.com>
2019-04-24 15:00:41 +0800

    Added drawer for opening categories.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, IconButton, Drawer } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import Categories from '../Categories'
 import Logout from '../../components/Logout'
 import './index.css'
 
 export default class Header extends React.Component {
 
+    state = {
+        open: false
+    }
+
+    toggleDrawer = (open) => {
+        this.setState({
+            open: open
+        })
+    }
+
     render() {
         return (
-            <AppBar position='static'>
+            <AppBar position='fixed' className='appbar'>
                 <Toolbar>
+                    <IconButton color='inherit' onClick={this.toggleDrawer}>
+                        <MenuIcon />
+                    </IconButton>
                     <Typography variant="h5" className="app-title" color="inherit" >
                         Civil Service Reviewer
                     </Typography>
@@ -18,6 +33,9 @@ export default class Header extends React.Component {
                         <Logout />
                     }
                 </Toolbar>
+                <Drawer open={this.state.open}>
+                    <Categories />
+                </Drawer>
             </AppBar>
         )
     }
